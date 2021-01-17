ca9f2b3741331dfa87afcd9e0a323991487505ba
Katherine Trammell <ktrammell.com>
2021-01-16 23:38:52 -0500

    Added questions to direct users
    Ability to add transactions

--- Program.cs
+++ Program.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace FirstBankOfSuncoast
 {
 
     class Transaction
     {
-        public string Account { get; set; }
+        public string AccountType { get; set; }
         public string TransactionType { get; set; }
-        public int TransactionValue { get; set; }
+        public string TransactionValue { get; set; }
 
     }
     class Program
@@ -18,33 +19,91 @@ namespace FirstBankOfSuncoast
         {
             var transactions = new List<Transaction>()
             {
-              new Transaction() {
-                Account = "Savings",
-                TransactionType = "Deposit",
-                TransactionValue = 25,
-              },
-              new Transaction() {
-                Account = "Checking",
-                TransactionType = "Deposit",
-                TransactionValue = 10,
-              },
-              new Transaction() {
-                Account = "Savings",
-                TransactionType = "Deposit",
-                TransactionValue = 25,
-              },
-              new Transaction() {
-                Account = "Savings",
-                TransactionType = "Withdrawal",
-                TransactionValue = 5,
-              },
-              new Transaction() {
-                Account = "Checking",
-                TransactionType = "Withdrawal",
-                TransactionValue = 20,
-              },
+                new Transaction() {
+                  AccountType = "Savings",
+                  TransactionType = "Deposit",
+                  TransactionValue = "25",
+                },
+                new Transaction() {
+                  AccountType = "Checking",
+                  TransactionType = "Deposit",
+                  TransactionValue = "10",
+                },
+                new Transaction() {
+                  AccountType = "Savings",
+                  TransactionType = "Deposit",
+                  TransactionValue = "25",
+                },
+                new Transaction() {
+                  AccountType = "Savings",
+                  TransactionType = "Withdrawal",
+                  TransactionValue = "5",
+                },
+                new Transaction() {
+                  AccountType = "Checking",
+                  TransactionType = "Withdrawal",
+                  TransactionValue = "20",
+                },
 
             };
+
+            static void BannerMessage(string message)
+            {
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("*******************************");
+                Console.WriteLine();
+                Console.WriteLine(message);
+                Console.WriteLine();
+                Console.WriteLine("*******************************");
+                Console.WriteLine();
+                Console.WriteLine();
+            };
+
+            BannerMessage("Welcome to First Bank of Suncoast");
+
+            Console.WriteLine("Would you like to make a Deposit or Withdrawal?");
+            var newTransactionType = Console.ReadLine();
+            // Console.WriteLine(newTransactionType);
+
+            if (newTransactionType == "Deposit" || newTransactionType == "deposit" || newTransactionType == "Checking" || newTransactionType == "checking")
+            {
+                Console.WriteLine("Please select an account:");
+                Console.WriteLine("- Checking");
+                Console.WriteLine("- Savings");
+                var newAccountType = Console.ReadLine();
+
+                if (newAccountType == "Savings" || newAccountType == "savings" || newAccountType == "Checking" || newAccountType == "checking")
+                {
+                    // Console.WriteLine(newAccountType);
+                    Console.WriteLine("Enter a $ amount");
+                    var newTransactionValue = Console.ReadLine();
+
+                    var newTransaction = new Transaction()
+                    {
+                        AccountType = newAccountType,
+                        TransactionType = newTransactionType,
+                        TransactionValue = newTransactionValue,
+                    };
+                    transactions.Add(newTransaction);
+
+                    foreach (var transaction in transactions)
+                    {
+                        Console.WriteLine($"{transaction.TransactionType} ${transaction.TransactionValue} into {transaction.AccountType} ");
+                    };
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Account Type entered, process canceled.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid Transaction Type Entered, process canceled.");
+            }
+
+
+
         }
     }
 }
