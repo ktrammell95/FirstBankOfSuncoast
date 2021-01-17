12996047f076c8713f7960a206d6918f76652989
Katherine Trammell <ktrammell.com>
2021-01-16 22:13:50 -0500

    Working on creating List and example data to start

--- Program.cs
+++ Program.cs
@@ -1,12 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FirstBankOfSuncoast
 {
-  class Program
-  {
-    static void Main(string[] args)
+
+    class Transaction
     {
-      Console.WriteLine("Welcome to C#");
+        public string Account { get; set; }
+        public string TransactionType { get; set; }
+        public int TransactionValue { get; set; }
+
     }
-  }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var transactions = new List<Transaction>()
+            {
+              new Transaction() {
+                Account = "Savings",
+                TransactionType = "Deposit",
+                TransactionValue = 25,
+              },
+              new Transaction() {
+                Account = "Checking",
+                TransactionType = "Deposit",
+                TransactionValue = 10,
+              },
+              new Transaction() {
+                Account = "Savings",
+                TransactionType = "Deposit",
+                TransactionValue = 25,
+              },
+              new Transaction() {
+                Account = "Savings",
+                TransactionType = "Withdrawal",
+                TransactionValue = 5,
+              },
+              new Transaction() {
+                Account = "Checking",
+                TransactionType = "Withdrawal",
+                TransactionValue = 20,
+              },
+
+            };
+        }
+    }
 }
